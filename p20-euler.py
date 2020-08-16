a6239365e8f89564db65e03817df67befa8fe7b2
Christian Kischy Haege <kischy@gmx.de>
2020-08-16 12:21:07 +0200

    Added test files and program  file

---
+++ p20-euler.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 12:20:22 2020
+
+@author: kisch
+"""
+
+
+
+problem_number = 20
+
+
+print("Calculation started")
+
+
+
+the_answer = 0
+
+print("The answer to the " + str(problem_number) + "th problem of ProjectEuler.Net is:",the_answer)
+
+
---
+++ p20-tests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 12:20:22 2020
+
+@author: kisch
+"""
+
+import unittest
+
+
+
+class TestTriangleMaximum(unittest.TestCase):  
+        
+    def test_something(self):
+        self.assertEqual(1, 1)
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=0)
